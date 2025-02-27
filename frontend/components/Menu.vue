9886d9dd0bbc782e50a73df937e3a2651e40893d
JIGEON PARK <jgp3620@gmail.com>
2025-02-27 10:22:45 +0900

    Update Menu component with new notice page link and responsive design improvements

--- frontend/components/Menu.vue
+++ frontend/components/Menu.vue
@@ -2,14 +2,14 @@
   <div>
     <!-- 메뉴 오버레이 -->
     <Transition name="fade">
-      <div v-if="isOpen" class="menu-overlay" @click="close">
-        <div class="menu-container">
+      <div v-if="isOpen" class="menu-overlay" @click="$emit('close')">
+        <div class="menu-container" @click.stop>
           <!-- 메뉴 패널 -->
           <Transition name="slide">
             <div v-if="isOpen" class="menu-panel">
               <div class="menu-header">
                 <h2>메뉴</h2>
-                <button class="close-button" @click="close">
+                <button class="close-button" @click="$emit('close')">
                   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M18 6L6 18M6 6l12 12" stroke="currentColor" stroke-width="2" stroke-linecap="round"/>
                   </svg>
@@ -17,14 +17,23 @@
               </div>
               
               <nav class="menu-items">
-                <NuxtLink to="/reading" class="menu-item" @click="close">
+                <!-- 공지사항 메뉴 아이템 추가 (가장 위에 배치) -->
+                <NuxtLink to="/notice" class="menu-item" @click="$emit('close')">
+                  <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M10.29 3.86L1.82 18a2 2 0 0 0 1.71 3h16.94a2 2 0 0 0 1.71-3L13.71 3.86a2 2 0 0 0-3.42 0z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12 9v4M12 17h.01" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                  </svg>
+                  <span>공지사항</span>
+                </NuxtLink>
+                
+                <NuxtLink to="/reading" class="menu-item" @click="$emit('close')">
                   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 6.25278V19.2528M12 6.25278C10.8321 5.47686 9.24649 5 7.5 5C5.75351 5 4.16789 5.47686 3 6.25278V19.2528C4.16789 18.4769 5.75351 18 7.5 18C9.24649 18 10.8321 18.4769 12 19.2528M12 6.25278C13.1679 5.47686 14.7535 5 16.5 5C18.2465 5 19.8321 5.47686 21 6.25278V19.2528C19.8321 18.4769 18.2465 18 16.5 18C14.7535 18 13.1679 18.4769 12 19.2528" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>
                   오늘일독
                 </NuxtLink>
                 
-                <NuxtLink to="/reading-plan" class="menu-item" @click="close">
+                <NuxtLink to="/reading-plan" class="menu-item" @click="$emit('close')">
                   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M9 5H7C6.46957 5 5.96086 5.21071 5.58579 5.58579C5.21071 5.96086 5 6.46957 5 7V19C5 19.5304 5.21071 20.0391 5.58579 20.4142C5.96086 20.7893 6.46957 21 7 21H17C17.5304 21 18.0391 20.7893 18.4142 20.4142C18.7893 20.0391 19 19.5304 19 19V7C19 6.46957 18.7893 5.96086 18.4142 5.58579C18.0391 5.21071 17.5304 5 17 5H15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                     <path d="M9 12H15M9 16H15" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
@@ -32,14 +41,14 @@
                   성경통독표
                 </NuxtLink>
                 
-                <NuxtLink to="/video" class="menu-item" @click="close">
+                <NuxtLink to="/video" class="menu-item" @click="$emit('close')">
                   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M15 10L19.5528 7.72361C20.2177 7.39116 21 7.87465 21 8.61803V15.382C21 16.1253 20.2177 16.6088 19.5528 16.2764L15 14M5 18H13C14.1046 18 15 17.1046 15 16V8C15 6.89543 14.1046 6 13 6H5C3.89543 6 3 6.89543 3 8V16C3 17.1046 3.89543 18 5 18Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>
                   하세나하시조
                 </NuxtLink>
                 
-                <NuxtLink to="/intro" class="menu-item" @click="close">
+                <NuxtLink to="/intro" class="menu-item" @click="$emit('close')">
                   <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                     <path d="M12 8V12M12 16H12.01M21 12C21 16.9706 16.9706 21 12 21C7.02944 21 3 16.9706 3 12C3 7.02944 7.02944 3 12 3C16.9706 3 21 7.02944 21 12Z" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
                   </svg>
@@ -78,23 +87,20 @@ const close = () => {
   -webkit-backdrop-filter: blur(4px);
   z-index: 40;
   display: flex;
-  justify-content: center;
+  justify-content: flex-end;
 }
 
 .menu-container {
   position: relative;
   width: 100%;
-  max-width: 768px;
+  max-width: 320px;
   height: 100%;
 }
 
 .menu-panel {
-  position: absolute;
-  top: 0;
-  right: 1rem;
-  bottom: 0;
+  position: relative;
   width: 100%;
-  max-width: 320px;
+  height: 100%;
   background: white;
   z-index: 50;
   padding: 1.5rem;
@@ -180,8 +186,7 @@ const close = () => {
 
 /* 모바일 대응 */
 @media (max-width: 640px) {
-  .menu-panel {
-    right: 0;
+  .menu-container {
     max-width: 100%;
   }
 }
