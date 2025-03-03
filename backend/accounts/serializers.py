6741a6b9e31c4494166ead453bdf0c7c87657ef7
JIGEON PARK <jgp3620@gmail.com>
2025-03-03 18:31:17 +0900

    Add is_staff field to UserSerializer for admin status detection

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,14 +1,21 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from .models import User
 
 User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
+    is_staff = serializers.SerializerMethodField()
+    
     class Meta:
         model = User
-        fields = ('id', 'username', 'nickname', 'profile_image')
-        read_only_fields = ('id',)
+        fields = ('id', 'username', 'nickname', 'profile_image', 'is_staff')
+        read_only_fields = ('id', 'is_staff')
+        
+    def get_is_staff(self, obj):
+        # superuser나 staff 권한이 있는 경우 admin으로 간주
+        return obj.is_superuser or obj.is_staff
 
 class RegisterSerializer(serializers.ModelSerializer):
     password = serializers.CharField(write_only=True)
