9822bff4b8bdac14b538135b696ebd2886801a48
JIGEON PARK <jgp3620@gmail.com>
2025-03-11 18:22:07 +0900

    Remove Share Test Menu Item from Menu Component
    - Delete the "공유 테스트" (Share Test) menu item from the Menu component
    - Remove associated SVG icon and menu link
    - Maintain clean and focused menu navigation structure

--- frontend/components/Menu.vue
+++ frontend/components/Menu.vue
@@ -30,19 +30,6 @@
                     <span>공지사항</span>
                   </NuxtLink>
 
-                  <!-- 공유 테스트 메뉴 아이템 추가 -->
-                  <NuxtLink to="/share-test" class="menu-item" @click="$emit('close')">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none"
-                      stroke="#000000" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
-                      <circle cx="18" cy="5" r="3"></circle>
-                      <circle cx="6" cy="12" r="3"></circle>
-                      <circle cx="18" cy="19" r="3"></circle>
-                      <line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
-                      <line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
-                    </svg>
-                    <span>공유 테스트</span>
-                  </NuxtLink>
-
                   <NuxtLink to="/reading" class="menu-item" @click="$emit('close')">
                     <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                       <path
