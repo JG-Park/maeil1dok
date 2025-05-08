617c927803e2bc233f48177cd724fd2516c9ccf5
JIGEON PARK <jgp3620@gmail.com>
2025-05-08 14:58:04 +0900

    Remove auth initialization from default layout on mount

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -6,10 +6,5 @@
 
 <script setup>
 import { onMounted } from 'vue'
-import { useAuthStore } from '@/stores/auth'
 
-const auth = useAuthStore()
-onMounted(async () => {
-  await auth.initializeAuth()
-})
 </script>
